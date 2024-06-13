114ad53a19a8d70e4afb478f8a02f19613c6643f
koogyeongmo <159003213+koogyeongmo@users.noreply.github.com>
2024-06-13 15:10:30 +0900

    made login screen

--- kiwi/src/reapp/src/pages/Login.js
+++ kiwi/src/reapp/src/pages/Login.js
@@ -128,8 +128,17 @@ const Login = ({ setIsLogin }) => {
 
           <div className={`login-container ${initialLoad ? 'flick-down' : ''} ${isAnimating ? 'flick-up' : ''}`}>
             <form onSubmit={handleSubmit} className="login-form">
-                <h2>{t('login')}</h2>
-                <div>
+                <div className='login-title'>{t('login')}</div>
+                <div className="login-input-container">
+
+                    <div className="login-icon-container">
+
+
+                        <svg className='login-icon' xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="gray" viewBox="0 0 16 16">
+                        <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
+                        </svg>
+                    </div>
+
                     <input
                         className="login-email-input"
                         type="email"
@@ -138,7 +147,14 @@ const Login = ({ setIsLogin }) => {
                         placeholder={t('email')}
                     />
                 </div>
-                <div>
+                <div className="login-input-container">
+                <div className="login-icon-container" >
+                    <svg className='login-icon' xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="gray" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.8 11.8 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7 7 0 0 0 1.048-.625 11.8 11.8 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.54 1.54 0 0 0-1.044-1.263 63 63 0 0 0-2.887-.87C9.843.266 8.69 0 8 0m0 5a1.5 1.5 0 0 1 .5 2.915l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99A1.5 1.5 0 0 1 8 5"/>
+                    </svg>
+
+                </div>
+
                     <input
                         className="login-password-input"
                         type="password"
