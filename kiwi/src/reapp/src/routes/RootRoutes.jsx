a25d7975bf3af4d4a10e59d473c721ca18f1b113
kss <holytype96@gmail.com>
2024-06-18 15:04:49 +0900

    리액트 파일 구조 변경
    기존 콤포넌트 실행 순서
    index.js -> app.js
    변경 순서
    index.js -> app.js -> RootRoutes.jsx

---
+++ kiwi/src/reapp/src/routes/RootRoutes.jsx
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+import Header from "../components/common/Header";
+import {Route, Routes, useLocation} from "react-router-dom";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import FileManagement from "../components/drive/FileManagement";
+import Main from "../pages/Main";
+import Chat from "../pages/Chat";
+import Calendar from "../pages/Calendar";
+import Drive from "../pages/Drive";
+import OAuth2RedirectHandler from "../jwt/OAuth2RedirectHandler";
+import Documents from "../pages/Documents";
+import Home from "../pages/Home";
+import Error from "../components/common/Error";
+
+const RootRoutes = () => {
+    return (
+        <Routes>
+            {/* 시작페이지이자 로그인페이지 */}
+            <Route path="/" element={<Login />}></Route>
+            {/* 임시 에러페이지 */}
+            <Route path="/error" element={<Error />} />
+            {/* 로그인 해야 접속가능한 페이지들 */}
+            <Route path="*" element={<SecondRouts />}></Route>
+        </Routes>
+    );
+};
+
+const SecondRouts = () => {
+
+    const location = useLocation();
+    const hideHeaderPaths = ['/chat', '/calendar', '/drive', '/documents'];
+    const shouldHideHeader = hideHeaderPaths.includes(location.pathname);
+
+    const [mounted, setMounted] = useState(false);
+
+    if(mounted){
+        return (
+            <>
+                {shouldHideHeader && <Header />}
+                <Routes>
+
+                    <Route path="/register" element={<Register/>}></Route>
+
+                    <Route path="/FileManagement" element={<FileManagement/>}></Route>
+                    <Route path="/main" element={<Main/>}></Route>
+                    <Route path='/chat' element={<Chat />} />
+                    <Route path='/calendar' element={<Calendar />} />
+                    <Route path='/drive' element={<Drive />} />
+                    <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler />} />
+                    <Route path="/documents" element={<Documents/>}></Route>
+                    <Route path="/home" element={<Home/>}></Route>
+                </Routes>
+            </>
+        );
+    }
+};
+
+
+export default RootRoutes;
