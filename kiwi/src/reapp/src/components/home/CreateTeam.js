faa327ca6c48e19ec95c8f0e22fda4a263e89718
koogyeongmo <159003213+koogyeongmo@users.noreply.github.com>
2024-06-19 16:06:06 +0900

    createteam 수정

--- kiwi/src/reapp/src/components/home/CreateTeam.js
+++ kiwi/src/reapp/src/components/home/CreateTeam.js
@@ -133,32 +133,29 @@ const CreateTeam = ({ onCreateTeam, toggleTeamView }) => {
           </div>
 
 
+          {formData.invitedMembers.length > 0 && (
             <div className='create-team-member-list-container'> 
                 <ul>
-                {formData.invitedMembers.map(member => (
-                        <li key={member.username} className="create-team-member">
-                            <img className='create-team-member-image' src={member.filepath} />
-
-                            <div className='create-team-member-info'>
-                              <div className='create-team-member-name-wrapper' >
-                                <div className='create-team-member-name'>{member.name}</div>
-                                <div className='create-team-invite-pending' > Invite Pending</div>
-                              </div>
-
-
-                              <div className='create-team-member-email'>{member.username}</div>
-                            </div>
-                            <div className='create-team-member-cancel'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" viewBox="0 0 16 16">
-                              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-                            </svg>
-
-                            </div>
-                        </li>
-                    ))}
-
+                  {formData.invitedMembers.map(member => (
+                    <li key={member.username} className="create-team-member">
+                      <img className='create-team-member-image' src={member.filepath} alt={`${member.name}'s avatar`} />
+                      <div className='create-team-member-info'>
+                        <div className='create-team-member-name-wrapper' >
+                          <div className='create-team-member-name'>{member.name}</div>
+                          <div className='create-team-invite-pending' > Invite Pending</div>
+                        </div>
+                        <div className='create-team-member-email'>{member.username}</div>
+                      </div>
+                      <div className='create-team-member-cancel'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" viewBox="0 0 16 16">
+                          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                        </svg>
+                      </div>
+                    </li>
+                  ))}
                 </ul>
             </div>
+          )}
 
         </div>
 
