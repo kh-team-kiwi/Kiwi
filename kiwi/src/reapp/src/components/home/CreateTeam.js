589f749282d80a9d968b1fdbcf8259561124f748
koogyeongmo <159003213+koogyeongmo@users.noreply.github.com>
2024-06-19 12:52:14 +0900

    changes

--- kiwi/src/reapp/src/components/home/CreateTeam.js
+++ kiwi/src/reapp/src/components/home/CreateTeam.js
@@ -10,18 +10,18 @@ const CreateTeam = ({ onCreateTeam, toggleTeamView }) => {
   const members = [
     {
         email: 'test1@gmail.com',
-        name: 'test1',
+        name: 'name1',
         image: ''
     },
     {
       email: 'test2@gmail.com',
-      name: 'test2',
+      name: 'name2',
       image: ''
     }
     ,
     {
       email: 'test3@gmail.com',
-      name: 'test3',
+      name: 'name3',
       image: ''
     }
 ];
@@ -57,32 +57,51 @@ const CreateTeam = ({ onCreateTeam, toggleTeamView }) => {
     <>
     <div className='create-team-title-container'>
       <div className='create-team-title'>
-        Create Team
+        Create a New Team
       </div> 
     </div>
         
     <div className='create-team-container' >  
       <form onSubmit={handleSubmit}>
         <div className='create-team-top'>
-          <div className='create-team-name-container' >
-            Team Name
-            <input
-              className="create-team-name-input"
-              name="teamName"
-              value={formData.teamName}
-              onChange={handleChange}
-            />
+          <div>
+            <div className='create-team-name-title'>
+              Team Name
+            </div>
+            <div className='create-team-name-container' >
+              <input
+                className="create-team-name-input"
+                name="teamName"
+                value={formData.teamName}
+                placeholder="Choose your team's name"
+
+                onChange={handleChange}
+              />
+            </div>
+
           </div>
-          <div className='create-team-search-members-container' >
-            Members 
-            <input
-              className="create-team-search-members-input"
-              name="invitedMembers"
-              placeholder='Search Members by Email'
-            //   value={formData.invitedMembers.join(', ')}
-              onChange={handleChange}
-            />
+
+          <div>
+            <div className='create-team-invite-members-title'>
+                Invite Members
+            </div>
+            <div className='create-team-invite-members-wrapper'>
+              <div className='create-team-invite-members-container' >
+                <input
+                  className="create-team-invite-members-input"
+                  name="invitedMembers"
+                  placeholder='Search members by email'
+                  onChange={handleChange}
+                />
+              </div>
+
+              <button className='create-team-invite-button'>
+                Invite
+              </button>
+
+            </div>
           </div>
+
 
             <div className='create-team-member-list-container'> 
                 <ul>
@@ -91,9 +110,19 @@ const CreateTeam = ({ onCreateTeam, toggleTeamView }) => {
                             <img className='create-team-member-image' src={member.image} />
 
                             <div className='create-team-member-info'>
-                              <div className='create-team-member-name'>{member.name}</div>
+                              <div className='create-team-member-name-wrapper' >
+                                <div className='create-team-member-name'>{member.name}</div>
+                                <div className='create-team-invite-pending' > Invite Pending</div>
+                              </div>
+
 
                               <div className='create-team-member-email'>{member.email}</div>
+                            </div>
+                            <div className='create-team-member-cancel'> 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" viewBox="0 0 16 16">
+                              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                            </svg>
+
                             </div>
                         </li>
                     ))}
@@ -114,7 +143,7 @@ const CreateTeam = ({ onCreateTeam, toggleTeamView }) => {
           </button>
 
           <button type="submit" className='create-team-create-button'>
-            Create
+            Create Team
           </button>
         </div>
       </form>
