8e104baf4977c289be9b08d699cf7c2f98f98d57
koogyeongmo <159003213+koogyeongmo@users.noreply.github.com>
2024-06-27 14:14:50 +0900

    calendar css 수정

--- kiwi/src/reapp/src/components/calendar/SchedulePopup.js
+++ kiwi/src/reapp/src/components/calendar/SchedulePopup.js
@@ -269,7 +269,7 @@ const SchedulePopup = ({ onClose, addEvent, calendars = [], setEvents }) => {
 
               <div className="event-description-container" >
                 <div>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" className='event-popup-icon' viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width='24' height='24' className='event-popup-icon' viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
                   </svg>
                 </div>
@@ -287,7 +287,7 @@ const SchedulePopup = ({ onClose, addEvent, calendars = [], setEvents }) => {
 
               <div className="event-location-container">
                 <div>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className='event-popup-icon' viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" className='event-popup-icon' viewBox="0 0 16 16">
                     <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
                   </svg>
                 </div>
