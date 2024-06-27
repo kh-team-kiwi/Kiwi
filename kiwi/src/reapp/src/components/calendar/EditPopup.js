53ed23aa4d0a3d3a6d7b7e0f01a24178bde27b0b
koogyeongmo <159003213+koogyeongmo@users.noreply.github.com>
2024-06-27 15:42:19 +0900

    calendar 수정

--- kiwi/src/reapp/src/components/calendar/EditPopup.js
+++ kiwi/src/reapp/src/components/calendar/EditPopup.js
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next';
 import axiosHandler from "../../jwt/axiosHandler";
 import {useLocation} from "react-router-dom";
 
-const EditPopup = ({ event, isOpen, onClose, setEvents,onCloseClose }) => {
+const EditPopup = ({ event, isOpen, onClose, setEvents }) => {
 
   const parseDateWithoutTimezone = (dateString) => {
     const [datePart, timePart] = dateString.split('T');
@@ -68,7 +68,6 @@ const EditPopup = ({ event, isOpen, onClose, setEvents,onCloseClose }) => {
         }));
 
         onClose();
-        onCloseClose();
       } else {
         alert(response.data.message);
       }
