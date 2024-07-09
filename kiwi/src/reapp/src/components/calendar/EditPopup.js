59bb2478056c396e793a75bac37def280e4f4b0f
koogyeongmo <159003213+koogyeongmo@users.noreply.github.com>
2024-07-09 09:10:02 +0900

    calendar 수정

--- kiwi/src/reapp/src/components/calendar/EditPopup.js
+++ kiwi/src/reapp/src/components/calendar/EditPopup.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import '../../styles/components/calendar/EditPopup.css';
 import { useTranslation } from 'react-i18next';
 import axiosHandler from "../../jwt/axiosHandler";
-import {useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { toast } from 'react-toastify';
+
 
 const EditPopup = ({ event, isOpen, onClose, setEvents }) => {
 
@@ -43,7 +45,11 @@ const EditPopup = ({ event, isOpen, onClose, setEvents }) => {
 
   const handleSave = async (e) => {
     e.preventDefault();
-    console.log("handleSave : ");
+
+    if (!title.trim()) {
+      toast.error("Title cannot be empty");
+      return;
+    }
 
     const updatedEvent = {
       ...event,
@@ -55,187 +61,175 @@ const EditPopup = ({ event, isOpen, onClose, setEvents }) => {
       calendar,
       color: selectedColor
     };
-    console.log("before",updatedEvent);
+    console.log("before", updatedEvent);
     try {
       const response = await axiosHandler.post("/api" + locate.pathname + "/update", updatedEvent);
       if (response.data.result) {
         const data = response.data.data;
-        console.log("after",data);
+        console.log("after", data);
         setEvents((prevEvents) => ({
           ...prevEvents,
           [data.calendar]: prevEvents[data.calendar].map(event =>
-              event.scheduleNo === data.scheduleNo ? data : event)
+            event.scheduleNo === data.scheduleNo ? data : event)
         }));
 
         onClose();
       } else {
-        alert(response.data.message);
+        toast.error(response.data.message);
       }
     } catch (error) {
       console.error("Failed to fetch schedule:", error);
     }
-
   };
 
   if (!isOpen) return null;
 
-  const handleScheduleEdit = async (updatedEvent) => {
-
-  }
-
   return (
     <div className="edit-popup-container">
-      {/* <div className='edit-popup-top'>
-        <div className='edit-popup-exit-container' onClick={onClose}>
-          <ExitIcon className='edit-popup-exit' />
-        </div>
-      </div> */}
       <div className='edit-popup-content'>
-      <form className="edit-popup-form" onSubmit={handleSave}>
-        <div className='edit-popup-calendar-option'>
-          <div className="edit-popup-slide-controls">
-            <div className="edit-popup-radio-wrapper">
-              <input 
-                type="radio" 
-                name="slide" 
-                id="edit-popup-personal" 
-                value="personal" 
-                checked={calendar === 'personal'} 
-                onChange={() => setCalendar('personal')} 
-              />
-              <input 
-                type="radio" 
-                name="slide" 
-                id="edit-popup-team" 
-                value="team" 
-                checked={calendar === 'team'} 
-                onChange={() => setCalendar('team')} 
-              />
-              <label htmlFor="edit-popup-personal" className="edit-popup-slide">{t('personal')}</label>
-              <label htmlFor="edit-popup-team" className="edit-popup-slide">{t('team')}</label>
-              <div className="edit-popup-slider-tab"></div>
+        <form className="edit-popup-form" onSubmit={handleSave}>
+          <div className='edit-popup-calendar-option'>
+            <div className="edit-popup-slide-controls">
+              <div className="edit-popup-radio-wrapper">
+                <input
+                  type="radio"
+                  name="slide"
+                  id="edit-popup-personal"
+                  value="personal"
+                  checked={calendar === 'personal'}
+                  onChange={() => setCalendar('personal')}
+                />
+                <input
+                  type="radio"
+                  name="slide"
+                  id="edit-popup-team"
+                  value="team"
+                  checked={calendar === 'team'}
+                  onChange={() => setCalendar('team')}
+                />
+                <label htmlFor="edit-popup-personal" className="edit-popup-slide">{t('personal')}</label>
+                <label htmlFor="edit-popup-team" className="edit-popup-slide">{t('team')}</label>
+                <div className="edit-popup-slider-tab"></div>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="edit-popup-title-container">
-          <div className='edit-popup-title-input-container'>
-            <input
-              type="text"
-              name="title"
-              placeholder={t('title')}
-              className="edit-popup-title-input"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            <div className="edit-popup-color-picker" onClick={() => setDropdownOpen(!dropdownOpen)}>
-              <div
-                className="edit-popup-color-circle"
-                style={{ backgroundColor: selectedColor }}
-              ></div>
-              {dropdownOpen && (
-                <ul className="edit-popup-color-dropdown-list">
-                  {presetColors.map((color, index) => (
-                    <li
-                      key={index}
-                      className="edit-popup-color-list-item"
-                      onClick={() => {
-                        setSelectedColor(color);
-                        setDropdownOpen(false);
-                      }}
-                    >
-                      <div
-                        className="edit-popup-color-circle"
-                        style={{ backgroundColor: color }}
-                      ></div>
-                    </li>
-                  ))}
-                </ul>
-              )}
+          <div className="edit-popup-title-container">
+            <div className='edit-popup-title-input-container'>
+              <input
+                type="text"
+                name="title"
+                placeholder={t('title')}
+                className="edit-popup-title-input"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              <div className="edit-popup-color-picker" onClick={() => setDropdownOpen(!dropdownOpen)}>
+                <div
+                  className="edit-popup-color-circle"
+                  style={{ backgroundColor: selectedColor }}
+                ></div>
+                {dropdownOpen && (
+                  <ul className="edit-popup-color-dropdown-list">
+                    {presetColors.map((color, index) => (
+                      <li
+                        key={index}
+                        className="edit-popup-color-list-item"
+                        onClick={() => {
+                          setSelectedColor(color);
+                          setDropdownOpen(false);
+                        }}
+                      >
+                        <div
+                          className="edit-popup-color-circle"
+                          style={{ backgroundColor: color }}
+                        ></div>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="edit-popup-description-container">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className='edit-popup-icon' viewBox="0 0 16 16">
-            <path fillRule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
-          </svg>
-          <div className='edit-popup-input-container'>
-            <input
-              name="description"
-              placeholder={t('description')}
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              className='edit-popup-description-input'
-            />
-          </div>
-        </div>
-
-        <div className="edit-popup-location-container">
-          <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" className='edit-popup-icon' viewBox="0 0 16 16">
-            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
-          </svg>
-          <div className='edit-popup-input-container'>
-            <input
-              type="text"
-              name="location"
-              className="edit-popup-location-input"
-              placeholder={t('location')}
-              value={location}
-              onChange={(e) => setLocation(e.target.value)}
-            />
-          </div>
-        </div>
-
-        <div className="edit-popup-duration-container">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className='edit-popup-icon' style={{ marginTop: '5px', marginLeft: '2px' }} viewBox="0 0 16 16">
-            <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
-            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
-          </svg>
-          <div className='edit-popup-duration-input-container'>
-            <div className="edit-popup-start-container">
+          <div className="edit-popup-description-container">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className='edit-popup-icon' viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
+            </svg>
+            <div className='edit-popup-input-container'>
               <input
-                type="time"
-                name="startTime"
-                value={startDate.slice(11, 16)}
-                onChange={(e) => setStartDate(startDate.slice(0, 11) + e.target.value)}
-                className='edit-popup-time-input'
-              />
-              <input
-                type="date"
-                name="startDate"
-                value={startDate.slice(0, 10)}
-                onChange={(e) => setStartDate(e.target.value + startDate.slice(10))}
-                className='edit-popup-date-input'
-              />
-            </div>
-            <div className="edit-popup-end-container">
-              <input
-                type="time"
-                name="endTime"
-                value={endDate.slice(11, 16)}
-                onChange={(e) => setEndDate(endDate.slice(0, 11) + e.target.value)}
-                className='edit-popup-time-input'
-              />
-              <input
-                type="date"
-                name="endDate"
-                value={endDate.slice(0, 10)}
-                onChange={(e) => setEndDate(e.target.value + endDate.slice(10))}
-                className='edit-popup-date-input'
+                name="description"
+                placeholder={t('description')}
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className='edit-popup-description-input'
               />
             </div>
           </div>
-        </div>
 
-        <div className="edit-popup-bottom-container">
-          <button type="button" className="edit-popup-cancel-button" onClick={onClose}>{t('cancel')}</button>
-          <button type="submit" className="edit-popup-save-button">{t('save')}</button>
-        </div>
-      </form>
+          <div className="edit-popup-location-container">
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" className='edit-popup-icon' viewBox="0 0 16 16">
+              <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
+            </svg>
+            <div className='edit-popup-input-container'>
+              <input
+                type="text"
+                name="location"
+                className="edit-popup-location-input"
+                placeholder={t('location')}
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+              />
+            </div>
+          </div>
 
+          <div className="edit-popup-duration-container">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className='edit-popup-icon' style={{ marginTop: '5px', marginLeft: '2px' }} viewBox="0 0 16 16">
+              <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+              <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
+            </svg>
+            <div className='edit-popup-duration-input-container'>
+              <div className="edit-popup-start-container">
+                <input
+                  type="time"
+                  name="startTime"
+                  value={startDate.slice(11, 16)}
+                  onChange={(e) => setStartDate(startDate.slice(0, 11) + e.target.value)}
+                  className='edit-popup-time-input'
+                />
+                <input
+                  type="date"
+                  name="startDate"
+                  value={startDate.slice(0, 10)}
+                  onChange={(e) => setStartDate(e.target.value + startDate.slice(10))}
+                  className='edit-popup-date-input'
+                />
+              </div>
+              <div className="edit-popup-end-container">
+                <input
+                  type="time"
+                  name="endTime"
+                  value={endDate.slice(11, 16)}
+                  onChange={(e) => setEndDate(endDate.slice(0, 11) + e.target.value)}
+                  className='edit-popup-time-input'
+                />
+                <input
+                  type="date"
+                  name="endDate"
+                  value={endDate.slice(0, 10)}
+                  onChange={(e) => setEndDate(e.target.value + endDate.slice(10))}
+                  className='edit-popup-date-input'
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="edit-popup-bottom-container">
+            <button type="button" className="edit-popup-cancel-button" onClick={onClose}>{t('cancel')}</button>
+            <button type="submit" className="edit-popup-save-button">{t('save')}</button>
+          </div>
+        </form>
       </div>
-
     </div>
   );
 };
