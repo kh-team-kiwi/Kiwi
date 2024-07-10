52961293042cbb3426f0f66a0f7e6c9abf742422
koogyeongmo <159003213+koogyeongmo@users.noreply.github.com>
2024-07-10 20:06:51 +0900

    언어 세팅 local storage 저장
    npm install i18next i18next-browser-languagedetector react-i18next

--- kiwi/src/reapp/src/index.js
+++ kiwi/src/reapp/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-//import './output.css'
 import { I18nextProvider } from 'react-i18next';
 import i18next from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import enTranslation from './languages/en.json';
 import koTranslation from './languages/ko.json';
@@ -14,40 +14,45 @@ import jaTranslation from './languages/ja.json';
 import zhTranslation from './languages/zh.json';
 import deTranslation from './languages/de.json';
 
-i18next.init({
-  interpolation: { escapeValue: false },
-  lng: 'en', 
-  resources: {
-    en: {
-      translation: enTranslation,
+i18next
+  .use(LanguageDetector) 
+  .init({
+    interpolation: { escapeValue: false },
+    fallbackLng: 'en', 
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      ko: {
+        translation: koTranslation,
+      },
+      es: {
+        translation: esTranslation,
+      },
+      fr: {
+        translation: frTranslation,
+      },
+      ja: {
+        translation: jaTranslation,
+      },
+      zh: {
+        translation: zhTranslation,
+      },
+      de: {
+        translation: deTranslation,
+      },
     },
-    ko: {
-      translation: koTranslation,
+    detection: {
+      order: ['localStorage', 'cookie', 'sessionStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
+      caches: ['localStorage'],
     },
-    es: {
-      translation: esTranslation,
-    },
-    fr: {
-      translation: frTranslation,
-    },
-    ja: {
-      translation: jaTranslation,
-    },
-    zh: {
-      translation: zhTranslation,
-    },
-    de: {
-      translation: deTranslation,
-    },
-  },
-});
+  });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <I18nextProvider i18n={i18next}>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-    </I18nextProvider>
+  </I18nextProvider>
 );
-
