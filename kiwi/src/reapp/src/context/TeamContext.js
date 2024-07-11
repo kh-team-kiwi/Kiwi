ceadea736796d8e6fe9b5aad803b35c412b89f91
koogyeongmo <159003213+koogyeongmo@users.noreply.github.com>
2024-07-11 15:10:15 +0900

    settings 수정

--- kiwi/src/reapp/src/context/TeamContext.js
+++ kiwi/src/reapp/src/context/TeamContext.js
@@ -1,26 +1,20 @@
-import React, { createContext, useState, useEffect } from 'react';
-import {setSessionItem} from "../jwt/storage";
+import React, { createContext, useState } from 'react';
 
-// 컨텍스트 생성
 const TeamContext = createContext(null);
 
-// 프로바이더 컴포넌트
 const TeamProvider = ({ children }) => {
-
-    const [teams,setTeams] = useState([]);
-
+    const [teams, setTeams] = useState([]);
     const [role, setRole] = useState(null);
     const [teamProfile, setTeamProfile] = useState('');
     const [headerRefresh, setHeaderRefresh] = useState(false);
 
-
     const joinTeam = (role) => {
         setRole(role);
-    }
+    };
 
     const leaveTeam = () => {
         setRole(null);
-    }
+    };
 
     const updateTeamProfile = (profile) => {
         setTeamProfile(profile);
@@ -30,9 +24,12 @@ const TeamProvider = ({ children }) => {
         setHeaderRefresh(prevState => !prevState);
     };
 
-
     return (
-        <TeamContext.Provider value={{ teams, setTeams, joinTeam, leaveTeam, role, teamProfile, updateTeamProfile , headerRefresh, triggerHeaderRefresh }}>
+        <TeamContext.Provider value={{
+            teams, setTeams, joinTeam, leaveTeam,
+            role, setRole, teamProfile, updateTeamProfile,
+            headerRefresh, triggerHeaderRefresh
+        }}>
             {children}
         </TeamContext.Provider>
     );
